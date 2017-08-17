b4a1c7644fd51dd80456bbaac4727c3e99b4e5f1
qianyubl <longjourney123@gmail.com>
2017-08-17 14:15:00 +0800

    add one abnormal UT case and make it pass

--- Source/Authorization.cpp
+++ Source/Authorization.cpp
@@ -3,7 +3,11 @@
 
 Permission CAuthorizationServer::getPermission(const  std::string name,const std :: string passwd)
 {
-    m_Authentication->login(name,passwd);
+    auto user = m_Authentication->login(name,passwd);
+    if(nullptr == user)
+    {
+        throw InvalidUser();
+    }
 
     return Permission();
 }
--- Test_modules/AuthorizationTestSuit.cpp
+++ Test_modules/AuthorizationTestSuit.cpp
@@ -14,3 +14,14 @@ TEST(AuthorizationTestSuit, verifyTheFuctionLogin)
     authorization.getPermission("user","password");
 }
 
+TEST(AuthorizationTestSuit, shouldThrowExceptionWhenUserNotExist)
+{
+    auto u = std::make_shared<User>(User{1,"user","123",EUserRole::E_READER, 1.1});
+    auto subObj = std::make_shared<AuthenticationMock>();
+    CAuthorizationServer authorization(subObj);
+    EXPECT_CALL(*subObj, login(_,_)).WillOnce(Return(nullptr));
+    ASSERT_THROW(authorization.getPermission("user","123"),InvalidUser);
+}
+
+
+
